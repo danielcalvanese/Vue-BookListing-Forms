00c9c2c4f25d5a1c295e823943d004f4c100d4e6
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-24 17:02:27 -0500

    Added module 4 tasks.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,21 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+
+    <hr />
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+
+    <ul>
+      <book-item
+        v-for="book in filteredBooks"
+        :key="book.id"
+        :book="book"
+      ></book-item>
+    </ul>
+
     <br />
     <hr />
     <book-form @addBook="appendBook"></book-form>
@@ -11,6 +26,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -25,23 +41,41 @@ export default {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
           finishedReading: true,
+          ownership: "borrowed",
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
           finishedReading: false,
+          ownership: "bought",
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
           finishedReading: true,
+          ownership: "borrowed",
         },
       ],
+
+      filters: ["bought", "borrowed"],
+      holding: "bought",
     };
   },
   components: {
     BookItem,
     BookForm,
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = (book) => {
+        return book.title.toLowerCase().match(this.searchInput.toLoserCase());
+      };
+
+      return _.filter(this.books, searchFilter);
+    },
   },
   methods: {
     appendBook(bookData) {
@@ -49,6 +83,7 @@ export default {
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
         finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership,
       });
     },
   },
